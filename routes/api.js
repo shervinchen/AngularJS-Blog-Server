41d060121bdc712777ccd846ae626c33434a6923
csdoker <csd758371536@qq.com>
2018-01-24 00:00:02 +0800

    modify

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,21 @@
-var marked = require('marked');
 var fs = require('fs');
+var marked = require('marked');
+var hljs = require('highlight.js');
+
+marked.setOptions({
+    renderer: new marked.Renderer(),
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: false,
+    smartLists: true,
+    smartypants: false,
+    xhtml: false,
+    highlight: function (code) {
+      return hljs.highlightAuto(code).value;
+    }
+});
 
 /*
  * Serve JSON to our AngularJS client
